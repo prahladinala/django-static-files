192dbd13b814c4c157ef88fff2e2b8badccabe9c
Prahlad Inala <prahlad2018@gmail.com>
2021-09-08 22:19:33 +0530

    Link CSS to Django > settings.py, main.html, static folder

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -110,6 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static') > OLD VERSION OF DJANGO
+    BASE_DIR / 'static' # NEW VERSION OF DJANGO
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
